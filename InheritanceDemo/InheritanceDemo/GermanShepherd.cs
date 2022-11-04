4c8d1748b58db56930f29fa786de38085cff5735
unknown <seth.dockendorf@gmail.com>
2022-11-04 14:45:55 -0500

    adding inheritance demo

---
+++ InheritanceDemo/InheritanceDemo/GermanShepherd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritanceDemo
+{
+    class GermanShepherd : Dog
+    {
+        public string Message { get; set; } = "I'm a goblin!";
+
+        public GermanShepherd(bool isAGoodBoy, string name) : base(isAGoodBoy, name) 
+        {
+
+        }
+    }
+}
---
+++ InheritanceDemo/InheritanceDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace InheritanceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GermanShepherd germanShepherd = new GermanShepherd(false, "Rufus");
+
+            Console.WriteLine($"I love my dog {germanShepherd.Name}");
+            Console.ReadKey();
+            Console.WriteLine($"OK {germanShepherd.Name}, I am going to leave this tasty ham on the table");
+
+            while(germanShepherd.IsAGoodBoy)
+            {
+                Console.WriteLine($"{germanShepherd.Name} sat obediently and did not eat the food off the table");
+                Console.ReadKey();
+            }
+            Console.WriteLine($"{germanShepherd.Name} ate the food. BAD BOY!!!!! >:(");
+            Console.ReadKey();
+
+
+            
+        }
+    }
+}
