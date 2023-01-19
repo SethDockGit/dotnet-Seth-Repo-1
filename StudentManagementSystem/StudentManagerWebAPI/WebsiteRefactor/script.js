288e2588a98cd35ab2596402032174cf0b590a94
unknown <seth.dockendorf@gmail.com>
2023-01-18 20:39:45 -0600

    Working on student management website

--- StudentManagementSystem/StudentManagerWebAPI/WebsiteRefactor/script.js
+++ StudentManagementSystem/StudentManagerWebAPI/WebsiteRefactor/script.js
@@ -11,29 +11,29 @@ function showStudentView(){
 
         students = data.students;
 
-        document.getElementById("content").innerHTML = 
-        '<p class="h2">Students</p><table class="table table-bordered w-auto" style="background-color:lightskyblue; margin:auto; text-align:center" id="studentsTable">';
+        document.getElementById("table").innerHTML = 
+        '<table class="table table-bordered w-auto" style="background-color:lightskyblue;" id="studentsTable">';
 
-        document.getElementById("studentsTable").innerHTML = `<thead><tr><th scope="col">ID</th>
+        document.getElementById("studentsTable").innerHTML = `</thead><tr><th scope="col">ID</th>
           <th scope="col">Name (Last, First)</th><th scope="col"></th></tr></thead>`;
 
         for(let i = 0; i < data.students.length; i++){
 
             document.getElementById("studentsTable").innerHTML += 
             `<tr><th scope="row">${data.students[i].id}</th><td>${data.students[i].name}</td>
-            <td id="student${[i]}"><button type="button" class="btn btn-secondary" onclick="viewStudent(${[i]})">View</button></td>`;
+            <td id="student${[i]}"><button type="button" class="btn btn-primary" onclick="viewStudent(${[i]})">View</button></td>`;
         } 
     });
 }
 function viewStudent(i){
 
 
-    document.getElementById(`student${[i]}`).innerHTML = `<td id="student${[i]}"><button type="button" class="btn btn-secondary" onclick="viewStudent(${[i]})">View</button></td>`;
-    document.getElementById(`student${[i]}`).innerHTML += `</br> Age: ${students[i].age} </br> Courses:`;
+    document.getElementById(`editor`).innerHTML = `<td id="student${[i]}"><button type="button" class="btn btn-secondary" onclick="viewStudent(${[i]})">View</button></td>`;
+    document.getElementById(`editor`).innerHTML += `</br> Age: ${students[i].age} </br> Courses:`;
 
     for (let j = 0; j < students[i].courses.length; j++){
                     
-        document.getElementById(`student${[i]}`).innerHTML += `- Course: ${students[i].courses[j].courseName}
+        document.getElementById(`editor`).innerHTML += `- Course: ${students[i].courses[j].courseName}
         CourseID: ${students[i].courses[j].courseId} 
         Professor: ${students[i].courses[j].professor}
         Description: ${students[i].courses[j].description}`;
@@ -44,5 +44,20 @@ function viewStudent(i){
 }
 function editStudent(i){
 
-
+    document.getElementById(`student${[i]}`).innerHTML += `<form>
+    <div class="mb-3">
+      <label for="exampleInputEmail1" class="form-label">Email address</label>
+      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+      <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+    </div>
+    <div class="mb-3">
+      <label for="exampleInputPassword1" class="form-label">Password</label>
+      <input type="password" class="form-control" id="exampleInputPassword1">
+    </div>
+    <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>`;
 }
